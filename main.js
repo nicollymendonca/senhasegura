a707bd69ceb68150f2298cf00e7eecdac1859996
nicollymendonca <164960912+nicollymendonca@users.noreply.github.com>
2025-07-03 21:45:06 -0300

    ok

---
+++ main.js
@@ -0,0 +1,31 @@
+const numeroSenha = document.querySelector('.parametro-senha_texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = 0123456789; 
+const simbolos = *!@%*?*;
+const botoes = document.querySelectorAll('.parametro-senha__botao");
+const campoSenha = document.querySelector('#campo-senha");
+const checkbox = document.querySelectorAll('.checkbox'); 
+const forcaSenha = document.querySelectorAll('forca');
+
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentatamanho;
+
+function diminuiTamanho(){
+    if(tamanhoSenha >1){
+        tamanhoSenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+
+function aumentatamanho(){
+    if(tamanhoSenha >20){
+        tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
